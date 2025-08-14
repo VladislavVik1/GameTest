63ffa1e8e8b9a82bc92bdab1d30c1cb9418703ec
VladislavVik1 <Dakraman1232@ukr.net>
2025-08-14 13:13:35 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT ? Number(process.env.PORT) : 4173,
-    allowedHosts: ['gametest-vbcv.onrender.com'] 
-  },
-
-  build: {
-    outDir: 'dist'
-  }
+  build: { outDir: 'dist' }
 })
